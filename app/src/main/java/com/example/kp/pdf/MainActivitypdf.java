ecc35c745acd08d133fd312849bbd7a8d3c5b633
lizalianita <44265100+lizalianita@users.noreply.github.com>
2019-02-07 21:18:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kp/pdf/MainActivitypdf.java
@@ -0,0 +1,13 @@
+package com.example.kp.pdf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivitypdf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activitypdf);
+    }
+}
